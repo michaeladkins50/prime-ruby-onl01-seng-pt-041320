b98240f8de506855f08e5f5d5990a9740d200bf7
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-21 15:46:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(numbers)# Add  code here!
+  prime_number = true
+  if numbers < 2
+    prime_number = false
+  elsif numbers == 2
+    prime_number = true
+  else
+    (2..(numbers - 1)).each do |n|
+      prime_number = false if numbers % n == 0
+    end
+  end
+    return prime_number
+end
