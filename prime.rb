2872944977156192dd74419a8c7eb870173a9e8d
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-21 15:59:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -5,9 +5,7 @@ def prime?(numbers)# Add  code here!
   elsif numbers == 2
     prime_number = true
   else
-    (2..(numbers - 1)).each do |n|
-      prime_number = false if numbers % n == 0
-    end
+    (2..(numbers - 1)).each {|n| prime_number = false if numbers % n == 0}
   end
     return prime_number
 end
